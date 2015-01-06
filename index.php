0909e51ba8106ff5c37e23d76e8ed7eec5e7ca5a
galacticevil <rory@galacticevil.org>
2015-01-06 22:38:02 +0200

    Added subtree successfully

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 echo '<h3>Codebase Subtree Test</h3>';
 echo '<p>We have an external Third Party Vendor lib which we will include as a Subtree</p>';
 
+require('vendor/dummy/dummy/test.php');
+
+$test = new Dummy\testClass('fred');
+echo '<pre>' . print_r($test->getVal(), 1) . '</pre>';
+
 
